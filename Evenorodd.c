43cf1618ab733b3c69ee71e71acd993e9c5bd73b
arunima <arunimachandran19@gmail.com>
2023-07-04 20:04:35 +0530

    task3

---
+++ Evenorodd.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    printf("Enter number");
+    scanf("%d", &a);
+
+   
+    if (a % 2 == 0) {
+        printf("The given number is EVEN");
+    }
+    else {
+        printf("The given number is ODD");
+    }
+    return 0;
+}
